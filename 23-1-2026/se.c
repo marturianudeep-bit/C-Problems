7514c1b5299c70a16f0aa4a012c3e97b2bba50ed
M.ANUDEEP <marturianudeep@gmail.com>
2026-01-23 19:28:23 +0530

    Add files via upload

---
+++ 23-1-2026/se.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+int main() { 
+    FILE *source, *destination; 
+    char ch; 
+    source = fopen("transactions.txt", "r"); 
+    destination = fopen("backup.txt", "w"); 
+    while ((ch = fgetc(source)) != EOF) 
+    { 
+        fputc(ch, destination); 
+    } 
+    printf("File copied successfully.\n"); 
+    fclose(source); 
+    fclose(destination); 
+
+    return 0;
+}
